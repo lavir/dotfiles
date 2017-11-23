4f6e1dac1d2fa8c0fceeba0a4b9ab788ebb56248
Alexandr Lukyanov <dev@lavir.com>
2017-11-23 11:58:40 +0200

    Add backup functionality and dry mode

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,26 +2,54 @@
 
 cd "$(dirname "${BASH_SOURCE}")";
 
-git pull origin master;
+# git pull origin master;
+
+DEST_PATH=/Users/lavir/temp-folder/dotfiles_desc
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+BACKUP_DIR_NAME="backups"
+BACKUP_PATH="$(pwd)/$BACKUP_DIR_NAME/$TIMESTAMP"
+
+function cleanupBackupDir() {
+	echo "Remove empty dirs in the $BACKUP_DIR_NAME"
+	find "$(pwd)/$BACKUP_DIR_NAME" -type d -depth 1 -empty -delete
+}
+
+function createBackupDir () {
+	echo "Make backup dir: $BACKUP_PATH"
+	mkdir $BACKUP_PATH
+}
 
 function doIt() {
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".osx" \
-		--exclude "bootstrap.sh" \
-		--exclude "README.md" \
-		--exclude "LICENSE-MIT.txt" \
-		-avh --no-perms . ~;
+	cleanupBackupDir;
+	createBackupDir;
+	
+	rsync --exclude-from=.excluded_from_sync \
+				--backup-dir=$BACKUP_PATH \
+				-avhb --no-perms . $DEST_PATH;
 	source ~/.bash_profile;
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-else
+function doItDry() {
+	rsync --exclude-from=.excluded_from_sync \
+		-avhn --no-perms . $DEST_PATH;
+	source ~/.bash_profile;
+}
+
+case "$1" in
+	--force|-f)
+			doIt;
+		;;
+	--dry)
+			doItDry;
+		;;
+*)
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
 	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		doIt;
 	fi;
-fi;
+	;;
+esac
+
 unset doIt;
+unset doItDry;
